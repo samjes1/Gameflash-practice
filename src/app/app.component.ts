8f6fab58b0f23cf5f9d2d915ecfdf56f12364df9
Samys Molina <samyjes4@gmail.com>
2024-09-09 09:55:58 -0400

    config folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BotonesComponent } from './components/botones/botones.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BotonesComponent  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
